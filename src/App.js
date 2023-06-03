8d9bb8987570c7bfedacff3286bd1cb4d82ccd5e
vuhaianh0907 <vuhaianh0907@gmail.com>
2023-06-03 14:13:47 +0700

    ex 11

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 
 import './App.css';
 import Navigation from './components/Navigation';
-import Player from './Players';
+import Main from './components/Main';
 import Footer from './components/Footer'
 function App() {
   return (
     <div className="App-header">
       <Navigation/>
-      <Player/>
+      <Main/>
       <Footer/>
     </div>
   );
